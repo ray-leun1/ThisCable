c75572fbe07087dc619125acc68537f492e54bb1
ray-leun1 <ray.leun1@gmail.com>
2020-01-03 14:01:07 -0800

    Get better timestamps

--- frontend/components/content/chat/message_index.jsx
+++ frontend/components/content/chat/message_index.jsx
@@ -7,6 +7,7 @@ class MessageIndex extends React.Component {
     super(props);
 
     this.renderMessages = this.renderMessages.bind(this);
+    this.dateParser = this.dateParser.bind(this);
   }
 
   componentDidMount() {
@@ -23,6 +24,35 @@ class MessageIndex extends React.Component {
     if (this.props.messages.length > 0) {
       let container = document.getElementsByClassName('message-index-container')[0];
       container.scrollTop = container.scrollHeight;
+    }
+  }
+
+  dateParser(date) {
+    const today = new Date();
+    const dateObject = new Date(date);
+    let timeString = dateObject.toLocaleTimeString('en-US', {
+      hour: 'numeric', minute: 'numeric'
+    });
+
+    if (today.getFullYear() === dateObject.getFullYear()
+      && today.getMonth() === dateObject.getMonth()
+      && today.getDate() - dateObject.getDate() <= 6) {
+      let dateDifference = today.getDate() - dateObject.getDate();
+      if (dateDifference === 0) return 'Today at ' + timeString;
+      if (dateDifference === 1) return 'Yesterday at ' + timeString;
+      if (dateDifference > 1) {
+        switch (today.getDay()) {
+          case 0: return 'Monday at ' + timeString;
+          case 1: return 'Tuesday at ' + timeString;
+          case 2: return 'Wednesday at ' + timeString;
+          case 3: return 'Thursday at ' + timeString;
+          case 4: return 'Friday at ' + timeString;
+          case 5: return 'Saturday at ' + timeString;
+          case 6: return 'Sunday at ' + timeString;
+        }
+      }
+    } else {
+      return dateObject.toLocaleDateString();
     }
   }
 
@@ -49,7 +79,7 @@ class MessageIndex extends React.Component {
                 {author.username}
               </div>
               <div className='message-timestamp'>
-                {`${message.created_at.split('T')[0]} at ${message.created_at.split('T')[1].slice(0, 5)}`}
+                {this.dateParser(message.created_at)}
               </div>
             </div>
             <div className='message-body'>
