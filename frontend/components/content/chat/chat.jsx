e52c2e3b162e4f274f9855263edbc61207be9f8e
ray-leun1 <ray.leun1@gmail.com>
2020-04-22 16:45:04 -0700

    Revert "Revert "More minor formatting fixes""
    This reverts commit 00f532c974234e9b16f1352f08815f9e1394622c.

--- frontend/components/content/chat/chat.jsx
+++ frontend/components/content/chat/chat.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, useHistory } from 'react-router-dom';
-import createMessage from '../../../actions/message_actions';
-import getCurrentChannel from '../../../actions/current_actions';
-import getUsers from '../../../actions/user_actions';
+import { createMessage } from '../../../actions/message_actions';
+import { getCurrentChannel } from '../../../actions/current_actions';
 import MessageIndex from './message_index';
 import MemberIndex from './member_index';
 
