6146df279afb5286720229a0fa39570759fb5310
ray-leun1 <ray.leun1@gmail.com>
2020-04-24 18:02:45 -0700

    Ready hooks conversion for testing

--- frontend/components/content/server_discovery/new_server_discovery.jsx
+++ frontend/components/content/server_discovery/new_server_discovery.jsx
@@ -1,59 +1,44 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ServerDiscoveryItem from './server_discovery_item';
 
 export default () => {
-  const servers = useSelector(state => state.entities.servers);
-  const [search, setSearch] = useState('');
+  const servers = Object.values(useSelector(state => state.entities.servers));
+  const currentUser = useSelector(state => state.entities.users[state.session.id]);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: '',
-      servers: this.props.getServers()
-    }
+  const [query, setQuery] = useState('');
 
-    this.renderServers = this.renderServers.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  renderServers() {
-    let servers = [];
-    let search = this.state.search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  const renderServers = () => {
+    let serverList = [];
+    let search = query.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     search = new RegExp(search, 'i');
 
-    this.props.servers.forEach(server => {
+    servers.forEach(server => {
       if (search.test(server.name)) {
-        servers.push(<ServerDiscoveryItem key={`server-discovery-item-${server.id}`} {...this.props} server={server} />);
+        serverList.push(<ServerDiscoveryItem key={`server-discovery-item-${server.id}`} currentUser={currentUser} server={server} />);
       }
     })
 
     return servers
   }
 
-  handleChange() {
-    return e => this.setState({ search: e.currentTarget.value })
-  }
-
-  render() {
-    return (<div className='server-discovery-container'>
-      <div className='header'>
-        Find new communities on thisCable
-      </div>
-      <div className='search-container'>
-        <input className='input'
-          type='text'
-          value={this.state.search}
-          placeholder='Try searching for a server name, I dare you'
-          onChange={this.handleChange()} />
-        {/* <button className='server-discovery-search-submit'
-          onClick={() => this.handleSubmit()}>
-          <i className="fas fa-search"></i>
-        </button> */}
-      </div>
-      <div className='server-list'>
-        {this.renderServers()}
-      </div>
-    </div>);
-  }
+  return (<div className='server-discovery-container'>
+    <div className='header'>
+      Find new communities on thisCable
+    </div>
+    <div className='search-container'>
+      <input className='input'
+        type='text'
+        value={query}
+        placeholder='Try searching for a server name, I dare you'
+        onChange={e => setQuery(e.currentTarget.value)} />
+      {/* <button className='server-discovery-search-submit'
+        onClick={() => this.handleSubmit()}>
+        <i className="fas fa-search"></i>
+      </button> */}
+    </div>
+    <div className='server-list'>
+      {renderServers()}
+    </div>
+  </div>);
 }
