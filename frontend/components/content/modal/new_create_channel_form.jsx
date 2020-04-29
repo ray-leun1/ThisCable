5998a453d54628a3edc5b1f8bbee028346c18769
ray-leun1 <ray.leun1@gmail.com>
2020-04-28 22:18:13 -0700

    Finish conversion of all function innards and do necessary rewrites

--- frontend/components/content/modal/new_create_channel_form.jsx
+++ frontend/components/content/modal/new_create_channel_form.jsx
@@ -1,14 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getChannels, getChannel, createChannel } from '../../../actions/channel_actions';
-import { getRoles } from '../../../actions/role_actions';
+import { getChannels, createChannel } from '../../../actions/channel_actions';
 import { createPermission } from '../../../actions/permission_actions';
 import { closeModal } from '../../../actions/modal_actions';
-import {
-  getCurrentUser,
-  getCurrentServer,
-  getCurrentChannel
-} from '../../../actions/current_actions';
 
 export default props => {
   const dispatch = useDispatch();
@@ -29,79 +23,66 @@ export default props => {
   const handleSubmit = () => {
     e.preventDefault();
 
-    this.props.createChannel({ name: this.state.name, server_id: this.state.server_id })
-      .then(channel => {
-        this.state.roles.forEach(role => {
-          if (this.state.private === 'selected') {
-            if (role.permit === 'selected') {
-              this.props.createPermission({ role_id: role.id, channel_id: channel.channel.id });
-            }
-          } else {
-            this.props.createPermission({ role_id: role.id, channel_id: channel.channel.id });
+    dispatch(createChannel({ name, server_id: serverId }))
+      .then(data => {
+        roles.forEach(role => {
+          if (private === 'selected' && role.permit === 'selected') {
+            dispatch(createPermission({ role_id: role.id, channel_id: data.channel.id }));
           }
         })
-        // this.props.getChannel(channel.channel.id);
-        this.props.getChannels(channel.channel.server_id);
-      })
 
-    this.props.closeModal();
+        dispatch(getChannels(data.channel.server_id));
+      });
+
+    dispatch(closeModal());
   }
 
   const handleOnEnter = e => {
     if (e.keyCode == 13 && e.shiftKey == false) {
       e.preventDefault();
-      this.handleSubmit(e);
+      handleSubmit(e);
     }
   }
 
-  const togglePrivate = () => {
-    if (this.state.private === 'deselected') {
-      this.setState({ private: 'selected' });
-    } else {
-      this.setState({ private: 'deselected' });
-    }
-  }
+  const togglePrivate = () => setPrivate(private === 'selected' ? 'deselected' : 'selected');
 
   const toggleRole = id => {
-    let roleIndex = this.state.roles.map(role => role.id).indexOf(id);
-    let roles = Array.from(this.state.roles);
+    let roleIdx = roles.map(role => role.id).indexOf(id);
+    let roleList = Array.from(roles);
 
-    if (roles[roleIndex].permit === 'deselected') {
-      roles[roleIndex].permit = 'selected';
-      this.setState({ roles: roles });
-    } else {
-      roles[roleIndex].permit = 'deselected';
-      this.setState({ roles: roles });
-    }
+    if (roleList[roleIdx].permit === 'deselected') roleList[roleIdx].permit = 'selected';
+    else roleList[roleIdx].permit = 'deselected';
+
+    setRoles(roleList);
   }
 
   const renderRoles = () => {
-    if (this.state.private === 'selected') {
+    if (private === 'selected') {
       return (<div className='create-channel-roles-container'>
         <div className='create-channel-label roles-container'>
           WHO CAN ACCESS THIS CHANNEL?
         </div>
         <div className='create-channel-private-role-container'>
           <div className='create-channel-private-role-selection'
-            key={`create-channel-private-role-selection-${this.state.roles[0].id}`}>
+            key={`create-channel-private-role-selection-${roles[0].id}`}>
             <div className='role-name'>
-              {this.state.roles[0].name}
+              {roles[0].name}
             </div>
-            <button className={`role-selector-${this.state.roles[0].id} ${this.state.roles[0].permit}`}
+            <button className={`role-selector-${roles[0].id} ${roles[0].permit}`}
               type='button'>
               <div className='role-slider'></div>
             </button>
           </div>
-          {this.state.roles.slice(1).map(role => {
+          {roles.slice(1).map(role => {
             return (<div className='create-channel-private-role-selection'
               key={`create-channel-private-role-selection-${role.id}`}>
               <div className='role-name'
-                onClick={() => this.toggleRole(role.id)}>
+                onClick={() => toggleRole(role.id)}>
                 {role.name}
               </div>
               <button className={`role-selector-${role.id} ${role.permit}`}
                 type='button'
-                onClick={() => this.toggleRole(role.id)}>
+                onClick={() => toggleRole(role.id)}>
                 <div className='role-slider'></div>
               </button>
             </div>)
